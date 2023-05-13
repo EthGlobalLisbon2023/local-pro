92160d5bdd3d66a5f5f58ec7e386812fd8a8fc46
“markclift” <mark.clift.uk@gmail.com>
2023-05-13 17:52:28 +0100

    checkout

---
+++ webapp/src/pages/api/checkout.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { setupContract } from "n/chain-utils";
+
+export default async function checkout(
+    request: NextApiRequest,
+    response: NextApiResponse
+) {
+    if (request.method !== 'POST') {
+        response.status(405).send({ message: 'Only POST requests are accepted' });
+        return;
+    }
+
+    const { checkoutLocation, taskDuration, roleAddress, roleAbi, userId } = request.body;
+
+    if (!roleAddress || !roleAbi || !userId) {
+        response.status(400).send({ message: 'Missing required parameters' });
+        return;
+    }
+
+    try {
+        const indexerContract = await setupContract(roleAddress, roleAbi);
+        
+        // Task completion logic: 
+        if (checkoutLocation == 1 && taskDuration == 1){
+            const allJobs = await indexerContract.completeTask(userId);
+
+            // TODO: Now issue a task completed credential!
+            // xxxxxxxxx
+
+            // TODO: Check if "Star Teacher" NFT has been unlocked and if so, auto-mint!
+            // xxxxxxxxx
+
+            response.status(200).send(allJobs);
+        }
+    } catch (error) {
+        response.status(500).send({ message: 'Server error', error: (error as any).message });
+    }
+}
