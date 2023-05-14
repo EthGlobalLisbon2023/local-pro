ccdb84f765e447215246b4c0f20beaf2a7e21332
sgerodes <sgerodes@gmail.com>
2023-05-14 04:21:01 +0100

    create qr code for verification

---
+++ webapp/src/pages/verify.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import QRCode from 'qrcode.react';
+
+const VERIFIER_BASE_URL = "localhost:8080";
+
+export default function Verify() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:8080/api/sign-in', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (!response.ok) {
+                console.error('Error:', response.statusText);
+                return;
+            }
+            const data = await response.json();
+            setData(data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            Verify
+            <QRCode value={JSON.stringify(data)} size={350}/>
+        </div>
+    );
+}
+
